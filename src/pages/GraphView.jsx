8dbcf33b0a2e0977f3ca85eb608b17aa79141328
VicenteVicente <vicente.calisto98@gmail.com>
2024-08-16 12:43:28 -0300

    feat: autosuggestion component

--- src/pages/GraphView.jsx
+++ src/pages/GraphView.jsx
@@ -1,27 +1,81 @@
 import FullscreenIcon from '@mui/icons-material/Fullscreen';
 import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
-import { Box, Paper, TextField, ToggleButton } from '@mui/material';
-import { useRef, useEffect, useState } from 'react';
+import {
+  Autocomplete,
+  Box,
+  Grid,
+  Paper,
+  TextField,
+  ToggleButton,
+  Typography,
+} from '@mui/material';
+import { debounce } from '@mui/material/utils';
+import parse from 'autosuggest-highlight/parse';
+import match from 'autosuggest-highlight/match';
+import * as d3Force from 'd3-force';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import ForceGraph2D from 'react-force-graph-2d';
 import { FullScreen, useFullScreenHandle } from 'react-full-screen';
 import { useResizeDetector } from 'react-resize-detector';
 import { useThemeContext } from '../context/ThemeContext';
-import * as d3Force from 'd3-force';
 
 const SearchBar = () => {
-  const onChangeTimeout = useRef(null);
+  const [value, setValue] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [options, setOptions] = useState([]);
 
-  const handleOnChange = (event) => {
-    if (onChangeTimeout.current) {
-      clearTimeout(onChangeTimeout.current);
-    }
-
-    if (event.target.value.length) {
-      onChangeTimeout.current = setTimeout(() => {
-        console.log('Search:', event.target.value);
-      }, 200);
-    }
+  const handleOnChange = (_event, newValue) => {
+    setOptions(newValue ? [newValue, ...options] : options);
+    setValue(newValue);
   };
+
+  const handleOnInputChange = (event, newInputValue) => {
+    setInputValue(newInputValue);
+  };
+
+  const textSearch = useMemo(
+    () =>
+      debounce((text, callback) => {
+        // TODO: MDB text search
+        const results = [
+          { id: 1 + text, label: text + 'A' },
+          { id: 2 + text, label: text + 'B' },
+          { id: 3 + text, label: text + 'C' },
+        ];
+        callback(results);
+      }, 400),
+    []
+  );
+
+  useEffect(() => {
+    let active = true;
+
+    if (!inputValue.length) {
+      setValue(null);
+      setOptions([]);
+      return;
+    }
+
+    textSearch(inputValue, (results) => {
+      if (active) {
+        let newOptions = [];
+
+        if (value) {
+          newOptions = [value];
+        }
+
+        if (results) {
+          newOptions = [...newOptions, ...results];
+        }
+
+        setOptions(newOptions);
+      }
+    });
+
+    return () => {
+      active = false;
+    };
+  }, [value, inputValue, textSearch]);
 
   return (
     <Paper
@@ -41,11 +95,55 @@ const SearchBar = () => {
         },
       })}
     >
-      <TextField
-        color="primary"
-        placeholder="Search for a node"
+      <Autocomplete
+        getOptionLabel={(option) => option.label}
+        getOptionKey={(option) => option.id}
+        filterOptions={(x) => x}
+        options={options}
+        value={value}
+        noOptionsText="No results"
         onChange={handleOnChange}
+        onInputChange={handleOnInputChange}
+        autoComplete
+        includeInputInList
+        filterSelectedOptions
         fullWidth
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            color="primary"
+            placeholder="Search for a node"
+            fullWidth
+          />
+        )}
+        renderOption={(props, option) => {
+          const { key, ...optionProps } = props;
+
+          const matches = match(option.label, inputValue);
+          const parts = parse(option.label, matches);
+          return (
+            <li key={key} {...optionProps}>
+              <Grid container sx={{ alignItems: 'center' }}>
+                <Grid item sx={{ width: '100%', wordWrap: 'break-word' }}>
+                  {parts.map((part, partIdx) => (
+                    <Box
+                      component="span"
+                      key={partIdx}
+                      sx={{
+                        fontWeight: part.highlight ? 'bold' : 'regular',
+                      }}
+                    >
+                      {part.text}
+                    </Box>
+                  ))}
+                </Grid>
+                <Typography variant="body2" color="text.secondary">
+                  {'Secondary text'}
+                </Typography>
+              </Grid>
+            </li>
+          );
+        }}
       />
     </Paper>
   );
@@ -100,11 +198,11 @@ export default function GraphView() {
   useEffect(() => {
     const graph = graphRef.current;
     if (graph) {
-      graph.d3Force('link', d3Force.forceLink().distance(60));
+      graph.d3Force('link', d3Force.forceLink().distance(30));
       graph.d3Force('center', d3Force.forceCenter().strength(0.05));
       graph.d3Force(
         'charge',
-        d3Force.forceManyBody().strength(-60).distanceMin(1).distanceMax(180)
+        d3Force.forceManyBody().strength(-30).distanceMin(1).distanceMax(150)
       );
     }
   }, [graphRef]);
