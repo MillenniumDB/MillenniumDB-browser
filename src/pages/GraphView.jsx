fd5faf251115f0ae64000c02598b609c07a3a488
VicenteVicente <vicente.calisto98@gmail.com>
2024-08-16 16:37:41 -0300

    progress on styles

--- src/pages/GraphView.jsx
+++ src/pages/GraphView.jsx
@@ -15,6 +15,63 @@ import ForceGraph2D from 'react-force-graph-2d';
 import { useResizeDetector } from 'react-resize-detector';
 import { useThemeContext } from '../context/ThemeContext';
 
+// Node size
+const NODE_VAL = 2; // Value of nodes
+const NODE_REL_SIZE = 4; // Ratio of node circle area per value unit
+const NODE_COLOR = 'cyan';
+const LINK_COLOR = 'orange';
+const TEXT_COLOR = '#fff';
+const TEXT_BG_COLOR = '#000';
+const FONT_SIZE = 14;
+
+const NodeCanvasObject = (node, ctx, globalScale) => {
+  const { x, y } = node;
+  if (node.isEdge) {
+    const sideOut = NODE_VAL * NODE_REL_SIZE;
+    const sideIn = sideOut / 1.25;
+
+    // Border
+    ctx.fillStyle = LINK_COLOR;
+    ctx.fillRect(x - sideOut / 2, y - sideOut / 2, sideOut, sideOut);
+
+    // Fill
+    ctx.fillStyle = ctx.canvas.style.backgroundColor;
+    ctx.fillRect(x - sideIn / 2, y - sideIn / 2, sideIn, sideIn);
+  } else {
+    const radiusOut = Math.sqrt(NODE_VAL) * NODE_REL_SIZE;
+    const radiusIn = radiusOut / 1.25;
+
+    // Border
+    ctx.fillStyle = NODE_COLOR;
+    ctx.beginPath();
+    ctx.arc(x, y, radiusOut, 0, 2 * Math.PI);
+    ctx.fill();
+
+    // Fill
+    ctx.beginPath();
+    ctx.arc(x, y, radiusIn, 0, 2 * Math.PI);
+    ctx.fillStyle = ctx.canvas.style.backgroundColor;
+    ctx.fill();
+  }
+
+  // Text
+  const fontSize = Math.max(FONT_SIZE / globalScale, 1);
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'top';
+  ctx.font = `${fontSize}px "Courier New", monospace`;
+
+  const textWidth = ctx.measureText(node.label).width;
+  ctx.fillStyle = TEXT_BG_COLOR;
+  ctx.fillRect(
+    x - textWidth / 2 - 1,
+    y + Math.sqrt(NODE_VAL) * NODE_REL_SIZE * 1.2 - 1,
+    textWidth + 2,
+    fontSize + 2
+  );
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.fillText(node.label, x, y + Math.sqrt(NODE_VAL) * NODE_REL_SIZE * 1.2);
+};
+
 const SearchBar = () => {
   const [value, setValue] = useState(null);
   const [inputValue, setInputValue] = useState('');
@@ -147,13 +204,22 @@ const SearchBar = () => {
 
 export default function GraphView() {
   const [graphData, setGraphData] = useState({
-    nodes: [...Array(100).keys()].map((i) => ({ id: i })),
-    links: [...Array(100).keys()]
-      .filter((id) => id)
-      .map((id) => ({
-        source: id,
-        target: Math.round(Math.random() * (id - 1)),
-      })),
+    nodes: [
+      { id: '1', label: 'Juan', isEdge: false },
+      { id: '2', label: 'Pablo', isEdge: false },
+      { id: '3', label: 'Diego', isEdge: false },
+      { id: '1->2', label: 'xsd:knows', isEdge: true },
+      { id: '1->3', label: 'xsd:knows', isEdge: true },
+      { id: '2->3', label: 'xsd:knows', isEdge: true },
+    ],
+    links: [
+      { source: '1', target: '1->2' },
+      { source: '1->2', target: '2' },
+      { source: '1', target: '1->3' },
+      { source: '1->3', target: '3' },
+      { source: '2', target: '2->3' },
+      { source: '2->3', target: '3' },
+    ],
   });
 
   const { width, height, ref } = useResizeDetector();
@@ -203,7 +269,17 @@ export default function GraphView() {
         height={height}
         backgroundColor={themeContext.darkMode ? '#242424' : '#dedede'}
         // Nodes
+        nodeRelSize={NODE_REL_SIZE}
+        nodeVal={NODE_VAL} // click/drag radius
+        nodeColor={null}
+        nodeLabel={null}
+        nodeCanvasObject={NodeCanvasObject}
+        nodeCanvasObjectMode={() => 'replace'}
         // Links
+        linkDirectionalArrowLength={(link) => (link.source.isEdge ? 4 : 0)}
+        linkDirectionalArrowRelPos={1}
+        linkColor={() => LINK_COLOR}
+        linkWidth={1}
         // Events
         onNodeClick={handleNodeClick}
         // Performance
