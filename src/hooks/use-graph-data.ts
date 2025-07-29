3c8829f4f930491d4c79d940cd3c457973869439
Vicente Calisto <37163344+VicenteVicente@users.noreply.github.com>
2025-07-29 14:24:05 -0300

    New graph explorer (#8)
    * chore(react-force-graph): graph explorer setup
    
    * use-graph-data hook
    
    * feat(graph): show node colors by labels
    
    * feat: context menu for graph
    
    * feat(graph): expand nodes connections on click
    
    * feat: toolbar + multiselect
    
    * feat(graph): add resizable sidebar component
    
    ---------
    
    Co-authored-by: Juan Pablo Sanchez <jp.sanchez@uc.cl>

---
+++ src/hooks/use-graph-data.ts
@@ -0,0 +1,140 @@
+import { useCallback, useRef, useState } from "react";
+import {
+  type LinkObject,
+  type GraphData,
+  type NodeObject,
+} from "react-force-graph-2d";
+
+export type NodeId = string;
+export type LinkId = string;
+
+export type MDBNode = {
+  id: NodeId;
+  name: string;
+  types?: string[];
+};
+
+export type MDBLink = {
+  id: LinkId;
+  name: string;
+  source: NodeId;
+  target: NodeId;
+};
+
+type UseGraphDataOptions = {
+  initialGraphData?: GraphData<MDBNode, MDBLink>;
+};
+
+export function useGraphData({
+  initialGraphData = { nodes: [], links: [] },
+}: UseGraphDataOptions = {}) {
+  const [graphData, setGraphData] =
+    useState<GraphData<MDBNode, MDBLink>>(initialGraphData);
+
+  const nodeMap = useRef(new Map<NodeId, NodeObject<MDBNode>>());
+  const linkMap = useRef(new Map<LinkId, LinkObject<MDBNode, MDBLink>>());
+  const outgoing = useRef(new Map<NodeId, Set<NodeId>>());
+  const incoming = useRef(new Map<NodeId, Set<NodeId>>());
+
+  const getNode = (id: string): NodeObject<MDBNode> | undefined => {
+    return nodeMap.current.get(id);
+  };
+
+  // updates the graphData state according to the maps
+  const update = useCallback(() => {
+    setGraphData({
+      nodes: Array.from(nodeMap.current.values()),
+      links: Array.from(linkMap.current.values()),
+    });
+  }, []);
+
+  // clear all the data structures
+  const clear = useCallback(() => {
+    nodeMap.current.clear();
+    linkMap.current.clear();
+    outgoing.current.clear();
+    incoming.current.clear();
+  }, []);
+
+  const addNode = useCallback((node: MDBNode) => {
+    const { id } = node;
+    if (!nodeMap.current.has(id)) {
+      nodeMap.current.set(id, node);
+      outgoing.current.set(id, new Set());
+      incoming.current.set(id, new Set());
+    }
+  }, []);
+
+  // TODO: fix remove link logic
+  const removeLink = useCallback((source: NodeId, target: NodeId) => {
+    const id: LinkId = `${source}->${target}`;
+    if (!linkMap.current.has(id)) return;
+
+    linkMap.current.delete(id);
+    outgoing.current.get(source)?.delete(target);
+    incoming.current.get(target)?.delete(source);
+  }, []);
+
+  const removeNode = useCallback(
+    (id: NodeId) => {
+      if (!nodeMap.current.has(id)) return;
+
+      // remove all its links
+      const out = outgoing.current.get(id) || new Set();
+      for (const target of out) {
+        removeLink(id, target);
+      }
+      const inc = incoming.current.get(id) || new Set();
+      for (const source of inc) {
+        removeLink(source, id);
+      }
+
+      // remove node
+      nodeMap.current.delete(id);
+      outgoing.current.delete(id);
+      incoming.current.delete(id);
+    },
+    [removeLink],
+  );
+
+  const addLink = useCallback((link: LinkObject<MDBNode, MDBLink>) => {
+    const { id, source, target } = link;
+
+    if (linkMap.current.has(id)) return;
+
+    // cannot create link because either source or target does not exist
+    if (!nodeMap.current.has(source) || !nodeMap.current.has(target)) return;
+
+    linkMap.current.set(id, link);
+    outgoing.current.get(source)?.add(target);
+    incoming.current.get(target)?.add(source);
+  }, []);
+
+  const getOutgoing = useCallback((id: NodeId): NodeObject<MDBNode>[] => {
+    return Array.from(outgoing.current.get(id) || []).map(
+      (id) => nodeMap.current.get(id)!,
+    );
+  }, []);
+
+  const getIncoming = useCallback((id: NodeId): NodeObject<MDBNode>[] => {
+    return Array.from(incoming.current.get(id) || []).map(
+      (id) => nodeMap.current.get(id)!,
+    );
+  }, []);
+
+  // Initialize data structures with initial graph data
+  initialGraphData.nodes.forEach((node) => addNode(node));
+  initialGraphData.links.forEach((link) => addLink(link));
+
+  return {
+    graphData,
+    update,
+    clear,
+    addNode,
+    removeNode,
+    addLink,
+    getOutgoing,
+    getIncoming,
+    getNode,
+  };
+}
--- src/routes/graph-explorer/index.tsx
+++ src/routes/graph-explorer/index.tsx
@@ -1,9 +1,499 @@
+import { useCallback, useEffect, useRef, useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
+import ForceGraph2D, {
+  type ForceGraphMethods,
+  type LinkObject,
+  type NodeObject,
+} from "react-force-graph-2d";
+import { Box, Menu, useMantineTheme } from "@mantine/core";
+import { useElementSize, useMediaQuery } from "@mantine/hooks";
+import {
+  useGraphData,
+  type MDBLink,
+  type MDBNode,
+  type NodeId,
+} from "@/hooks/use-graph-data";
+import { useGraphColorPalette } from "@/hooks/use-graph-color-palette";
+import { ContextMenu } from "@/components/graph-explorer/context-menu";
+import {
+  BoxSelection,
+  type OnBoxSelectionEnd,
+  type OnBoxSelectionMove,
+  type OnBoxSelectionStart,
+} from "@/components/graph-explorer/box-selection";
+import {
+  IconFileDescription,
+  IconNewSection,
+  IconPointer,
+  IconTrash,
+} from "@tabler/icons-react";
+import type { GraphNode } from "millenniumdb-driver";
+import { Toolbar } from "@/components/graph-explorer/toolbar";
+import { HEADER_HEIGHT, NAVBAR_WIDTH } from "@/layout/app-layout";
+import { Split } from "@gfazioli/mantine-split-pane";
+
+
+// Example graph data, replace with actual data fetching logic
+const graphDatabase = {
+  nodes: [
+    {
+      id: "n0",
+      name: "Chile",
+      types: ["Country"],
+    },
+    {
+      id: "n1",
+      name: "Santiago",
+      types: ["City"],
+    },
+    {
+      id: "n2",
+      name: "Pontificia Universidad Católica de Chile",
+      types: ["University", "Private"],
+    },
+    {
+      id: "n3",
+      name: "Universidad de Chile",
+      types: ["University", "Public"],
+    },
+    {
+      id: "n4",
+      name: "Juan Pérez",
+      types: ["Person", "Male", "Student"],
+    },
+    {
+      id: "n5",
+      name: "María González",
+      types: ["Person", "Female", "Student"],
+    },
+    {
+      id: "n6",
+      name: "Ana López",
+      types: ["Person", "Female", "Professor"],
+    },
+  ],
+  links: [
+    {
+      id: "l0",
+      source: "n1",
+      target: "n0",
+      name: "Located in",
+    },
+    {
+      id: "l1",
+      source: "n2",
+      target: "n1",
+      name: "Located in",
+    },
+    {
+      id: "l2",
+      source: "n3",
+      target: "n1",
+      name: "Located in",
+    },
+    {
+      id: "l3",
+      source: "n4",
+      target: "n2",
+      name: "Student at",
+    },
+    {
+      id: "l4",
+      source: "n5",
+      target: "n3",
+      name: "Student at",
+    },
+    {
+      id: "l5",
+      source: "n6",
+      target: "n2",
+      name: "Student at",
+    },
+    {
+      id: "l6",
+      source: "n4",
+      target: "n5",
+      name: "Friends with",
+    },
+    {
+      id: "l7",
+      source: "n5",
+      target: "n4",
+      name: "Friends with",
+    },
+    {
+      id: "l8",
+      source: "n4",
+      target: "n6",
+      name: "Friends with",
+    },
+    {
+      id: "l9",
+      source: "n6",
+      target: "n4",
+      name: "Friends with",
+    },
+  ],
+};
+
+export type CursorMode = "default" | "box-select";
+
+export type SelectionState = {
+  selectedNodeIds: Set<string>;
+  boxSelectedNodeIds: Set<string>;
+  boxSelectionActive: boolean;
+};
+
+function getOutgoingLinks(nodeId: string, graphDatabase) {
+  return graphDatabase.links.filter((link) => link.source === nodeId);
+}
+
+function getOutgoingNodes(nodeId: string, graphDatabase) {
+  const outgoingLinks = getOutgoingLinks(nodeId, graphDatabase);
+  return graphDatabase.nodes.filter((node) =>
+    outgoingLinks.some((link) => link.target === node.id),
+  );
+}
+
+function GraphExplorer() {
+  const fgRef = useRef<
+    | ForceGraphMethods<NodeObject<MDBNode>, LinkObject<MDBNode, MDBLink>>
+    | undefined
+  >(undefined);
+  const { ref: graphBoxRef, width, height } = useElementSize();
+
+  const theme = useMantineTheme();
+  const colorPalette = useGraphColorPalette();
+  const smallerThanXs = useMediaQuery(`(max-width: ${theme.breakpoints.xs})`);
+
+  const { graphData, addNode, addLink, update, getNode } = useGraphData({
+    initialGraphData: graphDatabase,
+  });
+
+  const [contextMenuState, setContextMenuState] = useState({
+    opened: false,
+    x: 0,
+    y: 0,
+  });
+
+  const [selection, setSelection] = useState<SelectionState>({
+    selectedNodeIds: new Set(),
+    boxSelectedNodeIds: new Set(),
+    boxSelectionActive: false,
+  });
+
+  const [cursorMode, setCursorMode] = useState<CursorMode>("default");
+
+  const typeColorMap = useRef(new Map<string, string>());
+
+  const getColorForType = useCallback(
+    (type?: string) => {
+      if (!type) return theme.colors.gray[6]; // default color
+      if (!typeColorMap.current.has(type)) {
+        const nextColor =
+          colorPalette[typeColorMap.current.size % colorPalette.length];
+        typeColorMap.current.set(type, nextColor);
+      }
+      return typeColorMap.current.get(type)!;
+    },
+    [colorPalette, theme.colors.gray],
+  );
+
+  const nodeCanvasObject = (node, ctx, globalScale) => {
+    const radius = 4;
+    const types = node.types ?? [];
+    const label = node.name;
+    const fontSize = 12 / globalScale;
+
+    // Draw the node as a pie chart
+    const slices = types.length > 0 ? types.length : 1;
+    const anglePerSlice = (2 * Math.PI) / slices;
+
+    for (let i = 0; i < slices; i++) {
+      const type = types[i] ?? undefined;
+      const color = getColorForType(type);
+
+      const startAngle = i * anglePerSlice;
+      const endAngle = startAngle + anglePerSlice;
+
+      ctx.beginPath();
+      ctx.moveTo(node.x, node.y);
+      ctx.arc(node.x, node.y, radius, startAngle, endAngle);
+      ctx.closePath();
+      ctx.fillStyle = color;
+      ctx.fill();
+    }
+
+    // Draw the border
+    ctx.beginPath();
+    ctx.arc(node.x, node.y, radius, 0, 2 * Math.PI);
+
+    if (selection.selectedNodeIds.has(node.id)) {
+      ctx.strokeStyle = "red";
+    } else if (selection.boxSelectedNodeIds.has(node.id)) {
+      ctx.strokeStyle = "blue";
+    } else {
+      ctx.strokeStyle = "white";
+    }
+
+    ctx.lineWidth = 1;
+    ctx.stroke();
+
+    // Draw the label
+    ctx.font = `${fontSize}px Sans-Serif`;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "top";
+    ctx.fillStyle = "black";
+    ctx.fillText(label, node.x, node.y + radius + 1);
+
+    // Set the background dimensions for the node
+    node.__bckgDimensions = [radius * 2, fontSize + 4];
+  };
+
+  const handleBackgroundClick = useCallback(() => {
+    setSelection((prev) => ({ ...prev, selectedNodeIds: new Set() }));
+  }, []);
+
+  const handleNodeClick = useCallback(
+    (node: NodeObject<MDBNode>, event: MouseEvent) => {
+      switch (cursorMode) {
+        case "default":
+          setSelection((prev) => {
+            if (event.ctrlKey || event.altKey || event.shiftKey) {
+              return {
+                ...prev,
+                selectedNodeIds: new Set([...prev.selectedNodeIds, node.id]),
+              };
+            } else {
+              return {
+                ...prev,
+                selectedNodeIds: new Set([node.id]),
+              };
+            }
+          });
+          break;
+        case "box-select":
+          break;
+      }
+    },
+    [cursorMode],
+  );
+
+  const handleNodeRightClick = useCallback(
+    (_node: NodeObject<MDBNode>, event: MouseEvent) => {
+      const { clientX, clientY } = event;
+      setContextMenuState((prev) => ({
+        ...prev,
+        opened: true,
+        x: clientX,
+        y: clientY,
+      }));
+    },
+    [],
+  );
+
+  const handleNodeDrag = useCallback(
+    (node: NodeObject<MDBNode>, translate: { x: number; y: number }) => {
+      if (selection.selectedNodeIds.has(node.id)) {
+        for (const selectedNodeId of selection.selectedNodeIds) {
+          if (selectedNodeId === node.id) continue;
+
+          const selectedNode = getNode(selectedNodeId);
+          if (!selectedNode) continue;
+
+          if (!selectedNode.x || !selectedNode.y) continue;
+          selectedNode.fx = selectedNode.x + translate.x;
+          selectedNode.fy = selectedNode.y + translate.y;
+        }
+      }
+    },
+    [getNode, selection.selectedNodeIds],
+  );
+
+  const handleNodeDragEnd = useCallback((node: NodeObject<MDBNode>) => {
+    // fix nodes after drag
+    node.fx = node.x;
+    node.fy = node.y;
+  }, []);
+
+  const handleBoxSelectionStart = useCallback<OnBoxSelectionStart>(
+    (_, event) => {
+      if (event.ctrlKey || event.shiftKey || event.altKey) return;
+      setSelection((prev) => ({ ...prev, selectedNodeIds: new Set() }));
+    },
+    [],
+  );
+
+  const handleBoxSelectionMove = useCallback<OnBoxSelectionMove>(
+    ({ minX, maxX, minY, maxY }) => {
+      if (!fgRef.current) return;
+
+      const xOffset = smallerThanXs ? 0 : NAVBAR_WIDTH;
+
+      const minGraphCoords = fgRef.current.screen2GraphCoords(
+        minX - xOffset,
+        minY - HEADER_HEIGHT,
+      );
+      const maxGraphCoords = fgRef.current.screen2GraphCoords(
+        maxX - xOffset,
+        maxY - HEADER_HEIGHT,
+      );
+
+      setSelection((prev) => {
+        const nextBoxSelectedNodeIds: Set<NodeId> = new Set();
+        for (const node of graphData.nodes) {
+          const { id, x, y } = node;
+
+          if (!x || !y) continue;
+
+          if (
+            minGraphCoords.x > x ||
+            maxGraphCoords.x < x ||
+            minGraphCoords.y > y ||
+            maxGraphCoords.y < y
+          ) {
+            // node is out of bounds
+            continue;
+          }
+
+          if (!prev.selectedNodeIds.has(node.id)) {
+            // node is not selected yet
+            nextBoxSelectedNodeIds.add(id);
+          }
+        }
+
+        return { ...prev, boxSelectedNodeIds: nextBoxSelectedNodeIds };
+      });
+    },
+    [graphData.nodes, smallerThanXs],
+  );
+
+  const handleBoxSelectionEnd = useCallback<OnBoxSelectionEnd>(() => {
+    setSelection((prev) => ({
+      selectedNodeIds: new Set([
+        ...prev.selectedNodeIds,
+        ...prev.boxSelectedNodeIds,
+      ]),
+      boxSelectedNodeIds: new Set(),
+      boxSelectionActive: false,
+    }));
+    setCursorMode("default");
+  }, []);
+
+  return (
+    <Box h="calc(100vh - var(--app-shell-header-height))">
+      <Split
+        h="100%"
+        color="var(--mantine-color-default-border)"
+        hoverColor="var(--mantine-primary-color-light-color)"
+        radius={0}
+        spacing={0}
+        variant="default"
+      >
+        <Split.Pane grow>
+          <Box ref={graphBoxRef} h="100%">
+            {selection.boxSelectionActive && (
+              <BoxSelection
+                onBoxSelectionStart={handleBoxSelectionStart}
+                onBoxSelectionMove={handleBoxSelectionMove}
+                onBoxSelectionEnd={handleBoxSelectionEnd}
+              />
+            )}
+            <ContextMenu
+              opened={contextMenuState.opened}
+              x={contextMenuState.x}
+              y={contextMenuState.y}
+              onClose={() =>
+                setContextMenuState((prev) => ({ ...prev, opened: false }))
+              }
+              menuItems={
+                selection.selectedNodeIds.size > 1 ? (
+                  <>
+                    {/* Selection menu */}
+                    <Menu.Item
+                      color="red"
+                      onClick={() => console.log("TODO: Selected nodes remove")}
+                      leftSection={<IconTrash size={14} />}
+                    >
+                      {"Remove selected nodes"}
+                    </Menu.Item>
+                  </>
+                ) : (
+                  <>
+                    {/* Single node menu */}
+                    <Menu.Item
+                      onClick={() => console.log("TODO: Node details")}
+                      leftSection={<IconFileDescription size={14} />}
+                    >
+                      {"Node details"}
+                    </Menu.Item>
+                    <Menu.Divider />
+                    <Menu.Item
+                      color="red"
+                      onClick={() => console.log("TODO: Node remove")}
+                      leftSection={<IconTrash size={14} />}
+                    >
+                      {"Remove node"}
+                    </Menu.Item>
+                  </>
+                )
+              }
+            />
+            <ForceGraph2D
+              ref={fgRef}
+              graphData={graphData}
+              width={width}
+              height={height}
+              linkDirectionalArrowLength={4}
+              nodeCanvasObject={nodeCanvasObject}
+              onBackgroundRightClick={() => {}}
+              linkColor={() => theme.colors.gray[4]}
+              linkDirectionalArrowRelPos={1}
+              onBackgroundClick={handleBackgroundClick}
+              onNodeClick={handleNodeClick}
+              onNodeRightClick={handleNodeRightClick}
+              onNodeDrag={handleNodeDrag}
+              onNodeDragEnd={handleNodeDragEnd}
+            />
+            <Toolbar
+              toolbarItems={[
+                {
+                  onClick: () => setCursorMode("default"),
+                  icon: IconPointer,
+                  label: "Default",
+                  cursorMode: "default",
+                },
+                {
+                  onClick: () => {
+                    setCursorMode("box-select");
+                    setSelection((prev) => ({ ...prev, boxSelectionActive: true }));
+                  },
+                  icon: IconNewSection,
+                  label: "Box selection",
+                  cursorMode: "box-select",
+                },
+              ]}
+              activeCursorMode={cursorMode}
+            />
+          </Box>
+        </Split.Pane>
+
+        <Split.Resizer />
+
+        <Split.Pane initialWidth="33%">
+          <Box h="100%" p="md">
+            <h2>Graph Explorer</h2>
+            <p>
+              Right-click on nodes to explore their details or remove them from
+              the graph.
+            </p>
+          </Box>
+        </Split.Pane>
+      </Split>
+    </Box>
+  );
+}
 
 export const Route = createFileRoute("/graph-explorer/")({
-  component: RouteComponent,
+  component: GraphExplorer,
 });
-
-function RouteComponent() {
-  return <div>Hello "/graph-explorer/"!</div>;
-}
